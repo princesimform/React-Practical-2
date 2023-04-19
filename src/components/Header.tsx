e70cb7f2df9d0bf982d0bcc9655f9cfabce3c74b
princesimform <prince.m@simformsolutions.com>
2023-04-19 09:56:43 +0530

    add live date

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,47 @@
 // StateLess Component
 function Header() {
+  var days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const today = new Date();
+  const dayName = days[today.getDay()];
+  const monthName = monthNames[today.getMonth()];
+
   return (
     <div>
       <div className='header'>
         <div className='time-stemp'>
           <div className='date-stemp'>
             <div className='date'>
-              <p>12</p>
+              <p>{today.getDate()}</p>
             </div>
             <div className='month-year'>
-              <p className='month'>JAN</p>
-              <p className='year'>2016</p>
+              <p className='month'>{monthName}</p>
+              <p className='year'>{today.getFullYear()}</p>
             </div>
           </div>
           <div className='day'>
-            <p>Tuesday</p>
+            <p>{dayName}</p>
           </div>
         </div>
       </div>
